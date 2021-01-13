d86e76b3aa3669ff9d453d77420302a337c4b8f9
Vitaliy Cherevko <vitaliycherevko@MBP-Cherevko.Dlink>
2021-01-13 14:43:44 +0300

    do not know how to solve on a next step

--- main.py
+++ main.py
@@ -1,63 +1,84 @@
+import random
 class grid:
-	def __init__(self,n = 3):
+    def __init__(self, n = 3):
         """Generation of the base table"""
-        
         self.n = n
-		self.table = [[((i*n + i/n + j) % (n*n) + 1) for j in range(n*n)] for i in range(n*n)]
+        self.table = [[((i*n + i/n + j) % (n*n) + 1) for j in range(n*n)] for i in range(n*n)]
 
     def transposing(self):
         """Transponing the whole grid"""
         self.table = map(list, zip(*self.table))
+        
+    def __del__(self):
+        pass
 
-	def __del__(self):
-		pass
-	
-	def show(self):
+    def show(self):
         """Showing the whole grid"""
-		for i in range(self.n*self.n):
-			print (self.table[i])
-    
+        for i in range(self.n*self.n):
+            print (self.table[i])
+
     def swap_rows(self):
         """Swap the two rows in a line of boxes"""
-        line1 = random.randrange(0,self.n,1)
-        line2 = random.randrange(0,self.n,1)
+        line1 = random.randrange(0, self.n, 1)
+        line2 = random.randrange(0, self.n, 1)
         while (line2 == line1):
-            line2 = random.randrange(0,self.n,1)
-        box = random.randrange(0,self.n,1)
+            line2 = random.randrange(0, self.n, 1)
+        box = random.randrange(0, self.n, 1)
         l1 = box*self.n + line1
         l2 = box*self.n + line2
         self.table[l1], self.table[l2] = self.table[l2], self.table[l1]
-    
+
     def swap_columns(self):
         """Swap the two columns in a line of boxes"""
         grid.transposing(self)
         grid.swap_rows(self)
         grid.transposing(self)  
 
-    def swap_rboxs(self):
-        """Swap the rows of boxes"""
-        box1 = random.randrange(0,self.n,1)
-        box2 = random.randrange(0,self.n,1)
+    def swap_row_boxes(self):
+        """Swap the row of boxes"""
+        box1 = random.randrange(0, self.n, 1)
+        box2 = random.randrange(0, self.n, 1)
         while (box2 == box1):
-            box2 = random.randrange(0,self.n,1)
+            box2 = random.randrange(0, self.n, 1)
         for i in range(0, self.n):
             l1, l2 = box1*self.n + i, box2*self.n + i
             self.table[l1], self.table[l2] = self.table[l2], self.table[l1]
 
-    def swap_cboxes(self):
+    def swap_column_boxes(self):
         """Swao the column of boxes"""
         grid.transposing(self)
-        grid.swap_rboxes(self)
+        grid.swap_row_boxes(self)
         grid.transposing(self)  
 
-    def mix(self,stp = 10):
+    def mix(self, stp = 10):
         mix_func = ['self.transposing()', 
 					'self.swap_rows()', 
 					'self.swap_columns()', 
-					'self.swap_rboxes()', 
-					'self.swap_cboxes()']
-        for i in xrange(1, stp):
-            id_func = random.randrange(0, len(mix_func),1)
+					'self.swap_row_boxes()', 
+					'self.swap_column_boxes()']
+        for i in range(1, stp):
+            id_func = random.randrange(0, len(mix_func), 1)
             eval(mix_func[id_func])
-a = grid(n = 3)
-a.show
+
+example = grid()
+example.mix()
+
+flook = [[0 for j in range(example.n ** 2)] for i in range(example.n ** 2)]
+iterator = 0
+difficulty =  example.n ** 4
+
+example.show()
+print("--------------------------")
+
+while iterator < exmaple.n ** 4
+    i, j = random.randrange(0, example.n ** 2, 1), random.randrange(0, example.n ** 2, 1)
+    if flook[i][j] == 0:
+        iterator += 1
+        flook[i][j] = 1
+
+        temp = example.table[i][j] #save the element if something goes wrong
+        example.table[i][j] = 0
+        difficulty -= 1
+        
+        table_solution = []
+        for solution in solver.
